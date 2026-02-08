6dfa4eb1b7974f25e02d91884ad3cde3058b6caa
richh-s <rahelsamson953@gmail.com>
2026-02-08 09:03:59 +0300

    move upload menu to side bar

--- src/components/layout/sidebar.tsx
+++ src/components/layout/sidebar.tsx
@@ -7,11 +7,13 @@ import {
   Package,
   MapPin,
   Boxes,
+  Upload,
 } from "lucide-react";
 import { cn } from "@/lib/utils";
 
 const navigation = [
   { name: "Dashboard", href: "/dashboard", icon: LayoutDashboard },
+  { name: "Upload Document", href: "/dashboard/organazation/documents", icon: Upload },
   { name: "My Shipments", href: "/dashboard/shipments", icon: Package },
   { name: "Containers", href: "/dashboard/containers", icon: Boxes },
   // { name: "Order History", href: "/history", icon: Clock },
